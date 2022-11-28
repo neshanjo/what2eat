a9d14c762027576cc78475942ce6a63b40062e0b
Johannes Schneider <johannes.schneider@htwg-konstanz.de>
2022-11-28 17:15:11 +0100

    Requesting all meals because loc2 might not always be present

--- src/main/java/de/schneider21/what2eat/meal/business/MensaKlService.java
+++ src/main/java/de/schneider21/what2eat/meal/business/MensaKlService.java
@@ -87,7 +87,7 @@ public class MensaKlService implements IMenuService {
                 .parse(API_URL)
                 .newBuilder()
                 .addQueryParameter("date", "all")
-                .addQueryParameter("loc", "2") // only requesting menus from "Ausgabe 2"
+//                .addQueryParameter("loc", "2") // uncomment to only request menus from "Ausgabe 2"
                 .addQueryParameter("format", "json");
         String url = urlBuilder.build().toString();
 
@@ -114,7 +114,7 @@ public class MensaKlService implements IMenuService {
                 .parse(API_URL)
                 .newBuilder()
                 .addQueryParameter("date", date)
-                .addQueryParameter("loc", "2") // only requesting menus from "Ausgabe 2"
+//                .addQueryParameter("loc", "2") // uncomment to only request menus from "Ausgabe 2"
                 .addQueryParameter("format", "json");
         String url = urlBuilder.build().toString();
 
